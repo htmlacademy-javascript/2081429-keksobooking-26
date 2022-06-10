88c669c45c2c71731d430dcaddce9801bb96359a
Keks <keks@htmlacademy.ru>
2022-06-10 19:17:37 +0300

    :heavy_check_mark: Сборка #2

--- 2/js/main.js
+++ 2/js/main.js
@@ -1,15 +1,19 @@
 //функция возвращает случайное целое число из диапазона
 function getRandomInt(min, max) {
-  if (max > min) {
-    return Math.floor(min + Math.random()*(max + 1 - min)); //для случаев, когда max < min или они равны, будет "undefined"
+  if (max < min) {
+    return getRandomInt(max, min);
   }
+  return Math.floor(min + Math.random()*(max + 1 - min));
 }
 getRandomInt(1, 3);
 
 //функция возвращает случайно число из диапазона с заданной точностью
 function getRandomNum(min, max, fraction=2) {
-  if (max > min) {
-    return (min + Math.random()*(max + 1 - min)).toFixed(fraction); //для случаев, когда max < min или они равны, будет "undefined"
+  if (max < min) {
+    return getRandomNum(max, min, fraction);
+  } else if (max === min) {
+    return max.toFixed(fraction);
   }
+  return (min + Math.random()*(max + 1 - min)).toFixed(fraction);
 }
 getRandomNum(1, 3);
