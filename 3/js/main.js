4cf70023e38811170af0094c9049d2c09938e549
Keks <keks@htmlacademy.ru>
2022-06-18 18:23:57 +0300

    :heavy_check_mark: Сборка #3

--- 3/js/main.js
+++ 3/js/main.js
@@ -1,16 +1,16 @@
-const NUMBEROFRENTALAD = 10;
-const ROOMSMAX = 4;
-const GUESTSMAX = 8;
+const NUMBER_OF_RENTAL_AD = 10;
+const ROOMS_MAX = 4;
+const GUESTS_MAX = 8;
 const PRICES = [1000, 50000];
-const LATMIN = 35.6500;
-const LATMAX = 35.7000;
-const LNGMIN = 139.7000;
-const LNGMAX = 139.8000;
+const LAT_MIN = 35.6500;
+const LAT_MAX = 35.7000;
+const LNG_MIN = 139.7000;
+const LNG_MAX = 139.8000;
 const TITLES = ['The best sea view', 'The biggest apartments in town', 'Apartments in a quiet location', 'Super modern apartments', 'Apartments on the massand floor'];
 const TYPES= ['palace','flat', 'house', 'bungalow', 'hotel'];
 const FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
-const CHECKINHOURS = ['12:00', '13:00', '14:00'];
-const CHECKOUTHOURS = ['12:00', '13:00', '14:00'];
+const CHECKIN_HOURS = ['12:00', '13:00', '14:00'];
+const CHECKOUT_HOURS = ['12:00', '13:00', '14:00'];
 const PHOTOS = [
   'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg',
   'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg',
@@ -34,9 +34,9 @@ const getRandomPositiveFloat = (a, b, digits = 1) => {
 };
 
 //число для адреса изображения
-const makeDoubleDigit = (digit) => (digit.toString().length === 2) ? `${digit}` : `0${digit}`;
+const makeDoubleDigit = (digit) => (digit >= 10) ? `${digit}` : `0${digit}`;
 
-//получить новый массив случайно длины
+//получить новый массив случайной длины
 const getRandomLengthArray = (array) => {
   const newArray = [];
   const newArrayLength = getRandomPositiveInteger(1, array.length);
@@ -54,23 +54,23 @@ const getRandomLengthArray = (array) => {
 const getRandomValueFromArray = (array) => array[getRandomPositiveInteger(1, array.length - 1)];
 
 //создать объявление
-const createRentalAdNearby = () => {
-  const latitude = getRandomPositiveFloat(LATMIN, LATMAX);
-  const longitude = getRandomPositiveFloat(LNGMIN, LNGMAX);
+const createRentalAdNearby = (index) => {
+  const latitude = getRandomPositiveFloat(LAT_MIN, LAT_MAX);
+  const longitude = getRandomPositiveFloat(LNG_MIN, LNG_MAX);
 
   return {
     author: {
-      avatar: `img/avatars/user${makeDoubleDigit(getRandomPositiveInteger(1, NUMBEROFRENTALAD))}.png`,
+      avatar: `img/avatars/user${makeDoubleDigit(index)}.png`,
     },
     offer: {
       title: getRandomValueFromArray(TITLES),
       address: `${latitude}, ${longitude}`,
       price: getRandomPositiveInteger(PRICES[0], PRICES[1]),
       type: getRandomValueFromArray(TYPES),
-      rooms: getRandomPositiveInteger(1, ROOMSMAX),
-      guests: getRandomPositiveInteger(1, GUESTSMAX),
-      checkin: getRandomValueFromArray(CHECKINHOURS),
-      checkout: getRandomValueFromArray(CHECKOUTHOURS),
+      rooms: getRandomPositiveInteger(1, ROOMS_MAX),
+      guests: getRandomPositiveInteger(1, GUESTS_MAX),
+      checkin: getRandomValueFromArray(CHECKIN_HOURS),
+      checkout: getRandomValueFromArray(CHECKOUT_HOURS),
       features: getRandomLengthArray(FEATURES),
       description: 'Description is not provided',
       photos: getRandomLengthArray(PHOTOS),
@@ -83,6 +83,12 @@ const createRentalAdNearby = () => {
 };
 
 //создание n-количества объявлений
-const createRentalAds = (number) => Array.from({length: number}, createRentalAdNearby);
+const createRentalAds = (number) => {
+  const newArray = [];
+  for (let i = 1; i <= number; i++) {
+    newArray.push(createRentalAdNearby(i));
+  }
+  return newArray;
+};
 
-createRentalAds(NUMBEROFRENTALAD);
+createRentalAds(NUMBER_OF_RENTAL_AD);
