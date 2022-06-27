6410d44399a9b1ea2a2ecd47200baa2b8a378c79
Keks <keks@htmlacademy.ru>
2022-06-27 15:41:50 +0300

    :heavy_check_mark: Сборка #6

---
+++ 6/js/main.js
@@ -0,0 +1,10 @@
+import {createRentalAdsFromTemplate} from './object-template.js';
+import {switchToInactiveState, switchToActiveState} from './search-form.js';
+
+const NUMBER_OF_RENTAL_AD = 1;
+
+createRentalAdsFromTemplate(NUMBER_OF_RENTAL_AD);
+
+switchToInactiveState();
+
+switchToActiveState();
